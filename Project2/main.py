6d35f1792d29eb09e68f9b010aac042c946d62ac
MSPigl <mattep96@gmail.com>
2019-04-19 18:17:10 -0400

    added kmeans

--- Project2/main.py
+++ Project2/main.py
@@ -1,9 +1,19 @@
+# utility
 import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer
 
-# load course IDs and trim out text label
-courseIDs = np.genfromtxt("course_descriptions.txt", usecols=(0));
-courseIDs = courseIDs[1:]
+# models
+from sklearn.cluster import KMeans, DBSCAN
 
-# load course descriptions and trim out text label
-courseDescs = np.genfromtxt("course_descriptions.txt", dtype=None, usecols=(1));
-courseDescs = courseDescs[1:]
+# metrics
+from sklearn.metrics import silhouette_score, adjusted_rand_score
+
+# load data from current directory
+docs = np.genfromtxt(".\\course_descriptions.txt", delimiter='\t', skip_header=1, dtype="str", usecols=(1))
+s_labels = np.genfromtxt(".\\school_labels.txt", delimiter='\t', skip_header=1, dtype="int", usecols=(1))
+f_labels = np.genfromtxt(".\\field_labels.txt", delimiter='\t', skip_header=1, dtype="int", usecols=(1))
+
+vec = CountVectorizer(stop_words=["the", "this", "course", "examines"], ngram_range=(2,2)).fit(docs)
+feature_array = vec.transform(docs)
+
+km = KMeans(n_clusters=3, random_state=42).fit(feature_array)
